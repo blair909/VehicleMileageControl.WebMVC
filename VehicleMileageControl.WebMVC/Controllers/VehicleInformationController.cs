15b09600b5cedf3f89ace72e778cf57e8cdcc807
blair909 <61510475+blair909@users.noreply.github.com>
2020-05-30 18:40:44 -0500

    Vehcile Information n-tiered Scaffold

---
+++ VehicleMileageControl.WebMVC/Controllers/VehicleInformationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VehicleMileageControl.WebMVC.Controllers
+{
+    public class VehicleInformationController : Controller
+    {
+        // GET: VehicleInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
