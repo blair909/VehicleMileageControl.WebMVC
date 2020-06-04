2623d9e63d26da1c4798739f34c25fa8eb74026c
blair909 <61510475+blair909@users.noreply.github.com>
2020-06-03 23:10:32 -0500

    changes for build init

--- VehicleMileageControl.WebMVC/Controllers/AccountController.cs
+++ VehicleMileageControl.WebMVC/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using VehicleMileageControl.Data;
 using VehicleMileageControl.WebMVC.Models;
 
 namespace VehicleMileageControl.WebMVC.Controllers
